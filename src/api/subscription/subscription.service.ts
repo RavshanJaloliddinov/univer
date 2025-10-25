4e7b9acb01f9ffe459bc60491f1b4b4cc202e92b
Biggle Psylocke <biggleps@ti-j5.skd.21-school.uz>
2025-10-25 21:40:58 +0500

    captcha

--- src/api/subscription/subscription.service.ts
+++ src/api/subscription/subscription.service.ts
@@ -34,6 +34,7 @@ export class SubscriptionService {
       `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${dto.captcha}`
     );
 
+    console.log(captchaResponse.data.score)
     if (!captchaResponse.data.success || captchaResponse.data.score < 0.5) {
       throw new BadRequestException('Captcha verification failed');
     }
